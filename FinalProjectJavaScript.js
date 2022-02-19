9bce026d68c3e76d7694d4918b616e658046d044
rwhite0236 <91112766+rwhite0236@users.noreply.github.com>
2022-02-19 16:41:49 -0500

    Add files via upload

---
+++ FinalProjectJavaScript.js
@@ -0,0 +1,37 @@
+//each function displays the game rating chosen by the user
+function userScore1(){
+	var ratingVal1 = document.getElementById("ratings1").value;
+	document.getElementById("value1").innerHTML = "Your score: " + ratingVal1 + "/10";
+	
+	
+}
+function userScore2(){
+	var ratingVal2 = document.getElementById("ratings2").value;
+	document.getElementById("value2").innerHTML = "Your score: " + ratingVal2 + "/10";
+	
+	
+}
+function userScore3(){
+	var ratingVal3 = document.getElementById("ratings3").value;
+	document.getElementById("value3").innerHTML = "Your score: " + ratingVal3 + "/10";
+	
+	
+}
+function userScore4(){
+	var ratingVal4 = document.getElementById("ratings4").value;
+	document.getElementById("value4").innerHTML = "Your score: " + ratingVal4 + "/10";
+	
+	
+}
+function userScore5(){
+	var ratingVal5 = document.getElementById("ratings5").value;
+	document.getElementById("value5").innerHTML = "Your score: " + ratingVal5 + "/10";
+	
+	
+}
+function userScore6(){
+	var ratingVal6 = document.getElementById("ratings6").value;
+	document.getElementById("value6").innerHTML = "Your score: " + ratingVal6 + "/10";
+	
+	
+}
